9f864fe580c01c19533c4b5a657174238293bc69
Paul Stinchcombe <paul@dragonsoftcommunications.com>
2023-05-06 23:38:18 +0800

    Update

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+require('dotenv').config()
+
 const app = require('express')()
-
-app.get('/', (req, res)=>{
-	res.send("<h1>Hello</h1>")
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB,
 })
 
-app.listen(3000, ()=>{
-	console.log("Listening on port 3000")
+connection.connect((err) => {
+  if (err) {
+    console.error(err.message)
+    process.exit(1)
+  }
+  app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
+  })
 })
 
+app.get('/:referral_code', (req, res) => {
+  const { refCode } = req.params
+  res.send('<h1>Hello</h1>')
+})
